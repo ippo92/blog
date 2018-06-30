a90193e05dcac93393037eab9334881d23f74f12
mohasalim <38589290+mohasalim@users.noreply.github.com>
2018-06-30 22:40:18 +0200

    Add files via upload
    Mise à jour de la page d'administration

--- view/dashboardView.php
+++ view/dashboardView.php
@@ -2,35 +2,93 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>Blog en php</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>espace d'administration</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Crete+Round" rel="stylesheet">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
 
-<!-- Barre de navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Acceuil <span class="sr-only">(current)</span></a>
-      </li>
-</nav>
-<body class="container-fluid">
-<h1 align="center">Blog en php!</h1>
+    <header>
+        <div class="wrappers">
+            <h1>Billet simple pour l'<span class="alaska">Alaska</span></h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="#articles">Articles</a></li>
+                    <li><a href="index.php/#contact">Contact</a></li>
+                    <li><a href="#">Administration</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 
+    <section id="fond">
+        <div class="container admin">
+            <div class="row">
+                <h3><strong>Liste des articles </strong>  <a href="" class=" btn btn-success "> Ajouter + </a> </h3>
+             
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Nom de l'article</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
 
-</body>
+while ($data = $posts->fetch())
+{
+?>
+                        <tr>
+                            <td><?php echo $data['title'] ?></td>
+                            <td><?php echo $data['creation_date']?></td>
+                            <td width="420">
+                            <a href="index.php?action=post&id=<?php echo $data['id']?>" class=" btn btn-success"> <span class="glyphicon glyphicon-eye-open"></span> Voir</a>
+                            <a href="index.php?action=getUpdatePost&id=<?php echo $data['id']?>" class="btn btn-primary"> Modifier</a>
+                            <a href="" class="btn btn-danger"> Supprimer</a>
+                            <a href="" class="btn btn-info"> Commentaires</a>
+                            </td>
+                        </tr>
+                      
+<?php
+}
+
+$posts->closeCursor(); // Termine le traitement de la requête
+
+?>
+
+                    </tbody>
+                </table>
+            </div>
+                    <div class="row">
+                        <h3><strong>Commentaires remontées : </strong></h3>
+                        <table class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Auteur</th>
+                                    <th>Date</th>
+                                    <th>Contenu</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Jean</td>
+                                    <td>21/01/1998</td>
+                                    <td width=""> Ceci est un test de commentaires !</td>
+                                    <td width="250">
+                                    <a href="" class="btn btn-primary"> Modifier</a>
+                                    <a href="" class="btn btn-danger"> Supprimer</a>
+                                    </td>
+                                </tr>
+                    </div>
+                </div>
 
 
