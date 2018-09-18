6fda65e82cd734f8dd986bf54f0ae4a922e232cc
mohasalim <38589290+mohasalim@users.noreply.github.com>
2018-09-18 13:57:24 +0200

    Delete dev.php

--- config/dev.php
+++
@@ -1,6 +0,0 @@
-<?php
-const DB_HOST = "mysql:host=localhost;dbname=test;charset=utf8";
-const DB_USER = "admin";
-const DB_PASS = "admin";
-const MAIL_USER = "mohasalim.gourari";
-const MAIL_PASS ="nnte865cv";
